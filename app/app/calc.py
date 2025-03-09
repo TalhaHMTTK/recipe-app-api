5d9266f8fc5e26e92b65b6617349d62bd1859d21
TalhaHMTTK <talhahmttk@gmail.com>
2025-03-09 11:57:55 -0700

    Setup a wait db command and also refine the code for linting

--- app/app/calc.py
+++ app/app/calc.py
@@ -2,9 +2,11 @@
 Calculator Functions
 """
 
+
 def add(x, y):
     """ add x and y two numbers"""
     return x + y
+
 
 def subtract(x, y):
     """ subtract x from y and return result."""
--- app/app/tests.py
+++ app/app/tests.py
@@ -5,6 +5,7 @@ from django.test import SimpleTestCase
 
 from app import calc
 
+
 class CalcTests(SimpleTestCase):
     """Test the Calc module"""
     def test_add_numbers(self):
